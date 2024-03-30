8ebe94d149fa1b235f06e73434306f0574e4a910
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-03-30 20:43:17 +0530

    LiveProject 30/03

--- LiveProject/project1/src/user/login.js
+++ LiveProject/project1/src/user/login.js
@@ -2,19 +2,28 @@ import { useState } from "react";
 import swal from "sweetalert";
 
 const Mylogin = () => {
+  let [email, pickEmail] = useState("");
+  let [pass, pickPass] = useState("");
 
-    let [email, pickEmail] = useState("");
-    let [pass, pickPass] = useState("");
-
-    const goLogin = () => {
-        if(email == "" || pass == ""){
-            swal("Invalid Input", "Empty Email or Password", "warning")
-        }
-        else{
-
-        }
+  const goLogin = () => {
+    if (email == "" || pass == "") {
+      swal("Invalid Input", "Empty Email or Password", "warning");
+    } else {
+      let url =
+        "http://localhost:1234/account?email=" + email + "&password=" + pass;
+      fetch(url)
+        .then((response) => response.json())
+        .then((userinfo) => {
+          if (userinfo.length > 0) {
+            localStorage.setitem("sellerid", userinfo[0].id);
+            localStorage.setitem("sellername", userinfo[0].fullname);
+            window.location.reload();
+          } else {
+            swal("Invalid Input", "Invalid or Not Exists", "warning");
+          }
+        });
     }
-
+  };
 
   return (
     <div className="container mt-4">
@@ -26,18 +35,26 @@ const Mylogin = () => {
               <i className="fa fa-lock"></i> Login
             </div>
             <div className="card-body">
-                <div className="mb-3">
-                    <label>Email ID</label>
-                    <input type="email" className="form-control" />
-                </div>
-                <div className="mb-3">
-                    <label>Password</label>
-                    <input type="password" className="form-control" />
-                </div>
+              <div className="mb-3">
+                <label>Email ID</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  onChange={(obj) => pickEmail(obj.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  onChange={(obj) => pickPass(obj.target.value)}
+                />
+              </div>
             </div>
             <div className="card-footer text-center">
               <button className="btn btn-danger" onClick={goLogin}>
-                Login<i className="fa fa-arrow-right"></i>
+                Login <i className="fa fa-arrow-right"></i>
               </button>
             </div>
           </div>
