bb08c519a0e622ba32c6127afb807bed98d82564
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-03-30 20:42:27 +0530

    LiveProject 30/03

--- LiveProject/project1/src/App.js
+++ LiveProject/project1/src/App.js
@@ -1,12 +1,12 @@
-import Mylogin from './user/login';
 import AdminApp from './admin/adminapp';
+import UserApp from './user/userapp';
 
 function App() {
-  return (
-    <>
-    <AdminApp/>
-    </>
-  );
+
+  if( localStorage.getItem("sellerid" == null))
+  return (<UserApp/>);
+  else
+  return (<AdminApp/>);
 }
 
 export default App;
